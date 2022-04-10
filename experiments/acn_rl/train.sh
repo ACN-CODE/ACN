9fb136fb274b88a2fbf1759ecd7fc825362fd4c5
Liu-Yuanqiu <1339255909@qq.com>
2022-04-10 13:02:04 +0800

    first

---
+++ experiments/acn_rl/train.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=1 python3 main.py --folder ./experiments/acn_rl --resume 45
+
+# 45 is the epoch number of the pretrained model
--- experiments/capsulexlan_rl/train.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python3 main.py --folder ./experiments/capsulexlan_rl --resume 45
-
-# 45 is the epoch number of the pretrained model
