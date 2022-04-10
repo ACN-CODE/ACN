614497279d538034265250c2a6e7c106d18c081a
Liu-Yuanqiu <45257466+Liu-Yuanqiu@users.noreply.github.com>
2022-04-10 12:50:37 +0800

    Update train.sh

--- experiments/capsulexlan/train.sh
+++ experiments/capsulexlan/train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python3 -m torch.distributed.launch --nproc_per_node=1 main.py --folder ./experiments/capsulexlan
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 main.py --folder ./experiments/capsulexlan
